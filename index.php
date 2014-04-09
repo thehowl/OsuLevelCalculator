664afae47102c0372fb9927909abf22e5785b77b
TheHowl <morganbaz@gmail.com>
2014-04-09 18:17:20 +0200

    The new style is completed!
    Holy shit!! It's completed!

--- index.php
+++ index.php
@@ -20,22 +20,19 @@ function ScoreLevelCalculator ($level,$currentScore = 0)
 		$part = $level - 100;
 		$result = 26931190829 + 100000000000 * $part;
 	}
-	else throw new Exception ("Holy shit, this is an error. Please write more info in the issues",111);
-
-	$result = abs($result);
+	else { 
+        throw new Exception ("Holy shit, this is an error. Please write more info in the issues",111);
+    }
 
 	return ($result - $currentScore);
 }
-if ($_POST['calct'] = 'api') {
-	$query = "https://osu.ppy.sh/api/get_user?k=" . $apikey . "&u=" . $_POST["nickname"] . "&m=" . $_POST["mode"];
+switch ($_GET["calct"]) {
+    case 'api': $query = "https://osu.ppy.sh/api/get_user?k=" . $apikey . "&u=" . $_GET["nickname"] . "&m=" . $_GET["mode"];
 	$json = file_get_contents($query);
 	$data = json_decode($json);
-	$actlvl = round($data[0]->level);
-	$lvlreach = $actlvl + 1;
-    $final = number_format(round(ScoreLevelCalculator ($level,$data[0]->total_score : 0)));
-}
-elseif ($_POST['calct'] = 'cl') {
-    $final = number_format(round(ScoreLevelCalculator ($_POST["leveltoreach"],0)));
+	$actlvl = round($data[0]->level) + 1;
+    $final = number_format(round(ScoreLevelCalculator ($actlvl,$data[0]->total_score))); break;
+    case 'cl': $final = number_format(round(ScoreLevelCalculator ($_GET["leveltoreach"],0))); break;
 }
 ?>
 <!DOCTYPE html>
@@ -45,26 +42,20 @@ elseif ($_POST['calct'] = 'cl') {
 <link href="stylev2.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div align="center" id="select"><a href="/">home</a> <a href="classic.php">classic</a> <a href="http://osu.ppy.sh/forum/t/199230/start=0">topic</a></div>
+<div align="center" id="select"><a href="/">home</a> <a href="#classicmode">classic</a> <a href="http://osu.ppy.sh/forum/t/199230/start=0">topic</a></div>
     <?
-if (isset($final)) {
-    echo '<h1>Results</h1>';
-    if ($_POST['calct'] = 'api' {
-        echo '<div class="txt">So, how much score needs ' . $_POST["nickname"] . ' to reach the next level?</div>';
-    }
-    else {
-        echo '<div class="txt">So, to reach level ' . $_POST["leveltoreach"] . ', you need a score of </div>';
-    }
-    echo '<br><div class="result">' . $final . '</div><br><div class="txt">do it again!</div><br>';
+switch ($_GET["calct"]) {
+    case 'api': echo '<h1>Results</h1> <div class="txt">So, how much score needs ' . $_GET["nickname"] . ' to reach the next level?</div><br><div class="result">' . $final . '</div><br><div class="txt">do it again!</div><br>';
     require 'form.html';
-    
-}
-else {
-    echo '<h1>osu! level calculator</h1>';
+    include 'footer.html'; break;
+    case 'cl':  echo '<div class="txt">So, to reach level ' . $_GET["leveltoreach"] . ', you need a score of </div><br><div class="result">' . $final . '</div><br><div class="txt">do it again!</div><br>';
+    require 'form.html';
+    include 'footer.html'; break;
+    default: echo '<h1>osu! level calculator</h1>';
     require 'form.html';
     echo '<br><br>';
-    include 'footer.html';
-}
+    include 'footer.html'; break;
+}   
 ?>
 </body>
 </html>
