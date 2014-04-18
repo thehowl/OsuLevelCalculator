daa53f8e349200fcbc2da7cce2c177be0783941e
TheHowl <morganbaz@gmail.com>
2014-04-18 13:11:57 +0200

    Started the language project: doing italian and english

---
+++ lang-en.php
@@ -0,0 +1,9 @@
+<?
+// English Language Source File
+// Made by TheHowl
+$olc-lang = array();
+$olc-lang["footer"] = "version 1.3 <br>Completely made by TheHowl<br> all the info you need about this can be found in the topic (link on the top of the page)";
+$olc-lang["example"] = "Ex."; // Abbreviation of Example
+$olc-lang["insnick"] = "insert nickname";
+$olc-lang["err"] = "Holy shit, this is an error. Please write more info in the issues";
+?>
