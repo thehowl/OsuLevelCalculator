4341fff846f8d95e38138c5e8d10f1b6b13c7007
DarkStoorM <b.sieprawski@op.pl>
2014-04-27 00:51:26 +0200

    Create lang-pl.php

---
+++ lang-pl.php
@@ -0,0 +1,29 @@
+<?php
+// Polish Language Source File
+// Translated by DarkstoorM
+$olclang = array();
+
+// Home and general things
+$olclang["ab-home"] = "główna";
+$olclang["ab-cl"] = "klasyczny";
+$olclang["ab-top"] = "temat";
+$olclang["footer"] = "wersja 1.3 <br>W całości stworzone przez TheHowl<br> wszelkie informacje możesz znaleźć w temacie na forum (link w nagłówku strony)";
+$olclang["err"] = "Cholera, wystąpił błąd. Napisz coś o nim i zgłoś go do mnie!";
+$olclang["title"] = "kalkulator poziomu osu!";
+$olclang["err2"] = "błąd";
+$olclang["cl-md"] = "Tryb klasyczny";
+$olclang["footer-ttl"] = "Stopka"; 
+
+// Form
+$olclang["example"] = "Np.";
+$olclang["insnick"] = "podaj nick";
+$olclang["mode"] = "tryb";
+$olclang["then"] = "a następnie";
+$olclang["submit"] = "wyślij!";
+$olclang["cl-tt"] = "podaj poziom, który chcesz osiągnąć"; 
+
+// Results
+$olclang["api-res"] = '<h1>Wyniki</h1> <div class="txt">Ile punktów ' . $_GET["nickname"] . ' potrzebuje, by zdobyć następny poziom?</div><br><div class="result">';
+$olclang["dia"] = '</div><br><div class="txt">policz jeszcze raz!</div><br>';
+$olclang["cl-res"] = '<h1>Wyniki</h1><div class="txt">By osiągnąć ' . $_GET["leveltoreach"] . ' poziom, potrzebujesz następującą ilość punktów </div><br><div class="result">';
+?>
